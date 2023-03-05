a36fa0f29533f3e54b27a15a40f35d90dcb1bd53
Kevin <kevdaviman.23@gmail.com>
2023-03-04 21:38:11 -0500

    add file Article.js inside folder models

---
+++ models/Article.js
@@ -0,0 +1,21 @@
+const {Schema, model} = require("mongoose");
+const ArticleSchema = Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    image: {
+        type: String,
+        default: "default.png"
+    },
+});
+
+module.exports = model("Article", ArticleSchema, "articles")
